314571ddb7403fbebd266db54532f9bd5896887a
Darren <i@charact3.com>
2019-01-23 01:41:52 +0800

    Bump to 0.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ URL = 'https://github.com/charact3/aiogetui'
 EMAIL = 'i@charact3.com'
 AUTHOR = 'Darren Char'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = '0.2'
+VERSION = '0.3'
 
 REQUIRED = ['six', 'aiohttp>3.0, <4.0']
 
